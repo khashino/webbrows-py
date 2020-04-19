a19b21bbcd311f74acb6a70a2aac49b4939283a3
khashayar norouzi <40909721+khashino@users.noreply.github.com>
2020-04-19 14:40:56 +0430

    Add files via upload

---
+++ tjonline.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import os
+
+def fileread():
+    f = open("myclass.conf", "r")
+    print(f.read())
+    
+
+def checkdate():
+    return
+
+def main():
+    browser = webdriver.Firefox()
+    browser.get('http://lms13.iauec.ac.ir/index.php')
+
+    userElem = browser.find_element_by_id('username')
+    userElem.send_keys('9511213732')  # admn no here
+    passwordElem = browser.find_element_by_id('password')
+    passwordElem.send_keys('0440721563')  # password here
+    loginElem = browser.find_element_by_class_name('submitBtn')
+    loginElem.click()
+
+
+
+if __name__ == '__main__':
+    fileread()
+    #checkdate()
+    #main()
